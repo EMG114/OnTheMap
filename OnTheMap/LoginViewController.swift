126b19e3fa5a3ca72fb8dc7f79a611d22d1c51b1
Erica Gutierrez <egutierrez411@gmail.com>
2018-03-03 23:07:59 -0500

    put api working

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -20,7 +20,8 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
    // apiTest()
     //  gettingAStudentLocation()
-        postAStudentLocation()
+       // postAStudentLocation()
+    putAStudentLocation()
        
 }
     
@@ -67,6 +68,28 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpBody = "{\"uniqueKey\": \"1234\", \"firstName\": \"John\", \"lastName\": \"Doe\",\"mapString\": \"Mountain View, CA\", \"mediaURL\": \"https://udacity.com\",\"latitude\": 37.386052, \"longitude\": -122.083851}".data(using: .utf8)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            if error != nil { // Handle error…
+                return
+            }
+            print(String(data: data!, encoding: .utf8)!)
+            
+        }
+        
+        task.resume()
+    }
+    
+    
+    func putAStudentLocation() {
+        let urlString = "https://parse.udacity.com/parse/classes/StudentLocation/DbYpQMR5FG"
+        let url = URL(string: urlString)
+        var request = URLRequest(url: url!)
+        request.httpMethod = "PUT"
+        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = "{\"uniqueKey\": \"1234\", \"firstName\": \"John\", \"lastName\": \"Doe\",\"mapString\": \"Cupertino, CA\", \"mediaURL\": \"https://udacity.com\",\"latitude\": 37.322998, \"longitude\": -122.032182}".data(using: .utf8)
         let session = URLSession.shared
         let task = session.dataTask(with: request) { data, response, error in
             if error != nil { // Handle error…
