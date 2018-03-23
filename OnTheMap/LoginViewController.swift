71eb6674f3d583dc16f7b5ca62f1a4854fab4813
Erica Gutierrez <egutierrez411@gmail.com>
2018-03-23 15:30:39 -0400

    working on login

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -18,31 +18,31 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-   // apiTest()
+//apiTest()
     //  gettingAStudentLocation()
        // postAStudentLocation()
-    putAStudentLocation()
+   // putAStudentLocation()
        
 }
     
-    func apiTest(){
-        
-        let urlString = "https://parse.udacity.com/parse/classes/StudentLocation?limit=100"
-        let url = URL(string: urlString)
-        var request = URLRequest(url: url!)
-        request.httpMethod = "GET"
-       request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
-        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
-        let session = URLSession.shared
-        let task = session.dataTask(with: request) { (data, response, error) in
-            
-    
-           // let parseJson = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
-            
-            print(String(data: data!, encoding: .utf8)!)
-        }
-        task.resume()
-    }
+//    func apiTest(){
+//
+//        let urlString = "https://parse.udacity.com/parse/classes/StudentLocation?limit=100"
+//        let url = URL(string: urlString)
+//        var request = URLRequest(url: url!)
+//        request.httpMethod = "GET"
+//       request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+//        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+//        let session = URLSession.shared
+//        let task = session.dataTask(with: request) { (data, response, error) in
+//
+//
+//           // let parseJson = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+//
+//            print(String(data: data!, encoding: .utf8)!)
+//        }
+//        task.resume()
+//    }
     
     func gettingAStudentLocation() {
         let urlString = "https://parse.udacity.com/parse/classes/StudentLocation"
@@ -80,9 +80,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         task.resume()
     }
     
-    func udacityAPI() {
-    
-    }
+   
     
     func api() {
         
@@ -131,7 +129,21 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func loginBUttonPressed(_ sender: Any) {
         if emailTextField.text?.isEmpty == false || passwordTextField.text?.isEmpty == false {
-            
+            var request = URLRequest(url: URL(string: "https://www.udacity.com/api/session")!)
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = "{\"udacity\": {\"username\": \"account@domain.com\", \"password\": \"password\"}}".data(using: .utf8)
+            let session = URLSession.shared
+            let task = session.dataTask(with: request) { data, response, error in
+                if error != nil { // Handle error…
+                    return
+                }
+                let range = Range(5..<data!.count)
+                let newData = data?.subdata(in: range) /* subset response data! */
+                print(String(data: newData!, encoding: .utf8)!)
+            }
+            task.resume()
         }
     }
     
